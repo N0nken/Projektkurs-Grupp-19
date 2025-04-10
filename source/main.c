6c19d4fa7ef17bb19288e973b7683075c89d33a8
KalebMarx <kaleb.marx03@gmail.com>
2025-04-10 19:59:33 +0200

    borde funka

--- source/main.c
+++ source/main.c
@@ -31,43 +31,43 @@ int main(int argv, char** args){
 
     Player *player1 = create_Player(create_Vector2(50, 50), create_Collider(create_Vector2(10, 10), create_Vector2(10, 10), 1), create_Collider(create_Vector2(10, 10), create_Vector2(10, 10), 1), create_Collider(create_Vector2(10, 10), create_Vector2(10, 10), 1), 100, 1, 1);
     //SDL_Rect *rect1 = Player_get_rect(player1);
-    float deltaTime = 1;
+    //float deltaTime = 1;
 
     while(isRunning){
         while(SDL_PollEvent(&event)){
             switch (event.type)
             {
                 case SDL_QUIT: isRunning = false;
-                case SDL_KEYDOWN:
-                    switch (event.key.keysym.scancode) {
-                        case SDL_SCANCODE_W:
-                        case SDL_SCANCODE_UP:
-                            Vector2_set_y(direction, -1.0f);
-                            break;
-                        case SDL_SCANCODE_S:
-                        case SDL_SCANCODE_DOWN:
-                            Vector2_set_y(direction, 1.0f);
-                            break;
-                        case SDL_SCANCODE_A:
-                        case SDL_SCANCODE_LEFT:
-                            Vector2_set_x(direction, -1.0f);
-                            break;
-                        case SDL_SCANCODE_D:
-                        case SDL_SCANCODE_RIGHT:
-                            Vector2_set_x(direction, 1.0f);
-                            break;
-                    }
+                
             }
         }
+
+        const Uint8 *keystates = SDL_GetKeyboardState(NULL);
+        if (keystates[SDL_SCANCODE_W] || keystates[SDL_SCANCODE_UP]) {
+            Vector2_set_y(direction, -1.0f);  // Gå upp
+        } else if (keystates[SDL_SCANCODE_S] || keystates[SDL_SCANCODE_DOWN]) {
+            Vector2_set_y(direction, 1.0f);   // Gå ner
+        } else {
+            Vector2_set_y(direction, 0.0f);   // Ingen rörelse i Y-led
+        }
+        
+        if (keystates[SDL_SCANCODE_A] || keystates[SDL_SCANCODE_LEFT]) {
+            Vector2_set_x(direction, -1.0f);  // Gå vänster
+        } else if (keystates[SDL_SCANCODE_D] || keystates[SDL_SCANCODE_RIGHT]) {
+            Vector2_set_x(direction, 1.0f);   // Gå höger
+        } else {
+            Vector2_set_x(direction, 0.0f);   // Ingen rörelse i X-led
+        }
+        
         //print_Vector2(direction);
         normalize(direction);
-        float speed = 5 * deltaTime;
+        float speed = 5;
         Vector2 *velocity = Vector2_const_multiplication(direction, speed);
         update_player_position(player1, velocity); // Anropa funktionen från movement.c
         destroy_Vector2(velocity);
         print_Vector2(Player_get_position(player1));
         
-        // reset direction
+        //reset direction
         Vector2_set_x(direction, 0.0f);
         Vector2_set_y(direction, 0.0f);
 
@@ -75,6 +75,7 @@ int main(int argv, char** args){
         SDL_RenderCopy(renderer, backgroundtexture, NULL, NULL);
         SDL_RenderCopy(renderer, playerTexture, NULL, Player_get_rect(player1));
         SDL_RenderPresent(renderer);
+        SDL_Delay(1000/60); // 60 fps
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
